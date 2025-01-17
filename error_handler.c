4b9f586b367112a19c37c2a3389de01ffb6a8d00
Marie Loeffer <mloeffer@z4r4p7.42lyon.fr>
2025-01-17 15:12:33 +0100

    fix : error from refactor + wip : check duplicate for many args

--- error_handler.c
+++ error_handler.c
@@ -6,34 +6,13 @@
 /*   By: mloeffer <mloeffer@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/16 12:19:49 by mloeffer          #+#    #+#             */
-/*   Updated: 2025/01/17 12:40:14 by mloeffer         ###   ########.fr       */
+/*   Updated: 2025/01/17 15:10:58 by mloeffer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-static int	check_is_only_digits_for_one_arg(char **argv)
-{
-	int	j;
-
-	j = 0;
-	while (argv[1][j])
-	{
-		while (argv[1][j] == ' ' || (argv[1][j] >= 9 && argv[1][j] <= 13))
-			j++;
-		if (argv[1][j] == '-' && !ft_isdigit(argv[1][j + 1]))
-			return (-1);
-		else if (argv[1][j] != '-' && !ft_isdigit(argv[1][j + 1]))
-			return (-1);
-		else if ((argv[1][j] == '-'
-			&& ft_isdigit(argv[1][j + 1])) || ft_isdigit(argv[1][j]))
-			j++;
-		j++;
-	}
-	return (0);
-}
-
-static int	check_is_only_digits_for_multiple_arg(int ac, char **av)
+static int	arg_has_only_digits(int ac, char **av)
 {
 	int	j;
 
@@ -43,8 +22,8 @@ static int	check_is_only_digits_for_multiple_arg(int ac, char **av)
 		j = 0;
 		while (av[ac][j])
 		{
-			if ((av[ac][j] == '-'
-				&& ft_isdigit(av[ac][j + 1])) || ft_isdigit(av[ac][j]))
+			if ((av[ac][j] == '-' && ft_isdigit(av[ac][j + 1]))
+			|| ft_isdigit(av[ac][j]))
 				{
 					j++;
 					continue;
@@ -62,100 +41,38 @@ static int	check_is_only_digits_for_multiple_arg(int ac, char **av)
 	return (0);
 }
 
-static int	check_dup_for_one_arg(char *arg, int count, int i, int *nums)
+static int	arg_has_no_duplicate(int ac, char **av)
 {
-	char	**tokens;
-	int		j;
+	int	i;
+	int tmp;
 
-	tokens = ft_split(arg, ' ');
-	while (tokens[count])
-		count++;
-	nums = malloc(sizeof(int) * count);
-	if (!nums)
-		return (-1);
-	while (++i < count)
-		nums[i] = ft_atoi(tokens[i]);
-	i = -1;
-	while (++i < count - 1)
-	{
-		j = i;
-		while (++j < count)
-		{
-			if (nums[i] == nums[j])
-			{
-				free_array(tokens);
-				return (-1);
-			}
-		}
-	}
-	free_array(tokens);
-	return (0);
-}
-
-/*static int	check_dup_for_more_arg(int ac, char **av, int count, int i, int *nums)
-{
-	char	**tokens;
-	int		j;
-
+	i = 0;
+	ac = ac - 1;
 	while (ac > 1)
 	{
-		//count = 0;
-		tokens = ft_split(av[ac], ' ');
-		while (tokens[count])
-			count++;
-		nums = malloc(sizeof(int) * count);
-		if (!nums)
-			return (-1);
-		while (++i < count)
-			nums[i] = ft_atoi(tokens[i]);
-		i = -1;
-		while (++i < count - 1)
+		tmp = ft_atoi(av[ac]);
+		while (i <= ac)
 		{
-			j = i;
-			while (++j < count)
-			{
-				if (nums[i] == nums[j])
-				{
-					free_array(tokens);
-					return (-1);
-				}
-			}
+			if (tmp == ft_atoi(av[i]))
+				return (-1);
+			i++;
 		}
-		free_array(tokens);
 		ac--;
 	}
 	return (0);
-}*/
+}
 
 int	error_handler(int argc, char **argv)
 {
-	int	size;
-	int	i;
-	int	*nb;
-
-	size = 0;
-	i = -1;
-	nb = NULL;
-	if ((argc < 2) || ((argc == 2)
-	&& (check_is_only_digits_for_one_arg(argv) == -1)) || ((argc > 2)
-	&& (check_is_only_digits_for_multiple_arg(argc, argv) == -1)))
+	if ((argc < 2) || (arg_has_only_digits(argc, argv) == -1))
 	{
 		write(2, "Error\n", 6);
 		return (-1);
 	}
-	if ((argc == 2) && (check_dup_for_one_arg(argv[1], size, i, nb) == -1))
+	if (arg_has_no_duplicate(argc, argv) == -1)
 	{
-		free(nb);
 		write(2, "Error\n", 6);
 		return (-1);
-	}
-	if (argc > 2)
-	// && (check_dup_for_more_arg(argc, argv, size, i, nb) == -1))
-	{
-		return (0);
-		/*free(nb);
-		write(2, "Error\n", 6);
-		return (-1);*/
 	}
 	return (0);
 }

f707e2018aba810e26a7bcc85a48d5c5aecc65b9
Marie Loeffer <mloeffer@z2r5p5.42lyon.fr>
2025-01-22 10:57:34 +0100

    fix : refactor for unused variables

--- main.c
+++ main.c
@@ -6,42 +6,33 @@
 /*   By: mloeffer <mloeffer@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/13 14:03:17 by mjzlak            #+#    #+#             */
-/*   Updated: 2025/01/22 10:28:54 by mloeffer         ###   ########.fr       */
+/*   Updated: 2025/01/22 10:56:31 by mloeffer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-int	push_swap(int argc, char **argv)
+/*int	push_swap(t_list *a, t_list *b)
 {
-	t_list	*a;
-	t_list	*b;
-	int		*value;
+	t_list	*current;
 
-	a = NULL;
-	b = NULL;
-	value = NULL;
-	if (argc == 1)
-		ft_split_to_list(argv[1], ' ', a);
-	else
+	current = a;
+	while (current)
 	{
-		while (--argc)
-		{
-			*value = ft_atoi(argv[argc]);
-			ft_lstadd_back(&a, ft_lstnew(value));
-		}
+		printf("a[%d] = %d\n", i, *(int *)(current->content));
+		current = current->next;
+		i++;
 	}
+	// 1. Parsing
+	// 2. Initialiser et remplir la pile a
+	// 3. Appeler la logique de tri
+	// 4. Afficher les instructions
 	return (0);
-}
+}*/
 
 int	main(int argc, char **argv)
 {
-	char	**array;
-	int		size;
-
-	array = NULL;
-	size = 0;
-	if (error_handler(array, size, argc, argv) == -1)
+	if (error_handler(NULL, 0, argc, argv) == -1)
 		return (-1);
 	return (0);
 }

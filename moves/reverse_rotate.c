2268d2a2cc1e210b07c44d66eb749fe1b3895838
Marie Loeffer <mloeffer@z4r2p6.42lyon.fr>
2025-01-27 09:00:48 +0100

    add : rra, rrb, rrr moves

--- moves/reverse_rotate.c
+++ moves/reverse_rotate.c
@@ -6,7 +6,7 @@
 /*   By: mloeffer <mloeffer@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/24 08:58:27 by mloeffer          #+#    #+#             */
-/*   Updated: 2025/01/24 11:37:34 by mloeffer         ###   ########.fr       */
+/*   Updated: 2025/01/27 08:54:20 by mloeffer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,46 +16,42 @@
 // The last element becomes the first one.
 void	rra(t_list **a)
 {
-	t_list	*tmp;
 	t_list	*last;
 	t_list	*before_last;
 
 	if (!*a || !(*a)->next)
 		return ;
-	tmp = *a;
-	*a = (*a)->next;
-	last = tmp;
-	before_last = tmp;
+	last = *a;
+	before_last = NULL;
 	while (last->next)
 	{
 		before_last = last;
 		last = last->next;
 	}
 	before_last->next = NULL;
-	last->next = tmp;
+	last->next = *a;
+	*a = last;
 }
 
 // Shift down all elements of stack b by 1.
 // The first element becomes the last one.
 void	rrb(t_list **b)
 {
-	t_list	*tmp;
 	t_list	*last;
 	t_list	*before_last;
 
 	if (!*b || !(*b)->next)
 		return ;
-	tmp = *b;
-	*b = (*b)->next;
-	last = tmp;
-	before_last = tmp;
+	last = *b;
+	before_last = NULL;
 	while (last->next)
 	{
 		before_last = last;
 		last = last->next;
 	}
 	before_last->next = NULL;
-	last->next = tmp;
+	last->next = *b;
+	*b = last;
 }
 
 // rra and rrb at the same time.

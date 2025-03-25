6eefedce90dbdee88fad2356b5499b26ff4bede2
Marie Loeffer <mloeffer@z4r5p4.42lyon.fr>
2025-03-25 16:44:53 +0100

    fix : errors from int overflow and when one nb is given with letter inside

--- utils/error_handler.c
+++ utils/error_handler.c
@@ -6,7 +6,7 @@
 /*   By: mloeffer <mloeffer@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/16 12:19:49 by mloeffer          #+#    #+#             */
-/*   Updated: 2025/03/25 12:09:59 by mloeffer         ###   ########.fr       */
+/*   Updated: 2025/03/25 15:35:49 by mloeffer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ int	print_and_return_error(char *message, int return_value)
 	return (return_value);
 }
 
-static int	arg_has_only_digits(int ac, char **av)
+static int	args_has_only_digits(int ac, char **av, int overflow)
 {
 	int	i;
 	int	j;
@@ -31,7 +31,7 @@ static int	arg_has_only_digits(int ac, char **av)
 		{
 			if (av[i][j] == '-' || av[i][j] == '+')
 			{
-				if (!ft_isdigit(av[i][j + 1]))
+				if (av[i][j + 1] == '\0' || !ft_isdigit(av[i][j + 1]))
 					return (-1);
 			}
 			else if (!ft_isdigit(av[i][j]) && av[i][j] != ' '
@@ -39,27 +39,61 @@ static int	arg_has_only_digits(int ac, char **av)
 				return (-1);
 			j++;
 		}
+		ft_atoi(av[i], &overflow);
+		if (overflow)
+			return (-1);
 		i--;
 	}
 	return (0);
 }
 
-static int	arg_has_no_duplicate(int i, int ac, char **av)
+static int	args_has_no_duplicate(int i, int ac, char **av, int overflow)
 {
 	int		j;
 	int		tmp;
 
 	while (i < ac)
 	{
-		tmp = ft_atoi(av[i]);
+		tmp = ft_atoi(av[i], &overflow);
+		if (overflow)
+			return (-1);
 		j = i + 1;
 		while (j < ac)
 		{
-			if (tmp == ft_atoi(av[j]))
+			if (tmp == ft_atoi(av[j], &overflow))
 				return (-1);
 			j++;
 		}
 		i++;
+	}
+	return (0);
+}
+
+static int	arg_has_only_digits(int ac, char **av, int overflow)
+{
+	int	i;
+	int	j;
+
+	i = ac - 1;
+	while (i >= 0)
+	{
+		j = 0;
+		while (av[i][j])
+		{
+			if (av[i][j] == '-' || av[i][j] == '+')
+			{
+				if (av[i][j + 1] == '\0' || !ft_isdigit(av[i][j + 1]))
+					return (-1);
+			}
+			else if (!ft_isdigit(av[i][j]) && av[i][j] != ' '
+				&& (av[i][j] < 9 || av[i][j] > 13))
+				return (-1);
+			j++;
+		}
+		ft_atoi(av[i], &overflow);
+		if (overflow)
+			return (-1);
+		i--;
 	}
 	return (0);
 }
@@ -75,8 +109,10 @@ int	error_handler(char **array, int size, int ac, char **av)
 			return (print_and_return_error("Error\n", -1));
 		while (array[size])
 			size++;
-		if (size == 0 || arg_has_no_duplicate(0, size, array) == -1
-			|| arg_has_only_digits(size, array) == -1)
+		if (size == 0 || args_has_no_duplicate(0, size, array, 0) == -1
+			|| args_has_only_digits(size, array, 0) == -1
+			|| (size == 1 && arg_has_only_digits(size, array, 0) == -1)
+			|| (size == 1 && args_has_no_duplicate(0, size, array, 0) == -1))
 		{
 			free_array(array);
 			return (print_and_return_error("Error\n", -1));
@@ -84,8 +120,8 @@ int	error_handler(char **array, int size, int ac, char **av)
 		free_array(array);
 		return (0);
 	}
-	if ((arg_has_no_duplicate(1, ac, av) == -1)
-		|| arg_has_only_digits(ac, av) == -1 || av[1][0] == '\0')
+	if ((args_has_no_duplicate(1, ac, av, 0) == -1)
+		|| args_has_only_digits(ac, av, 0) == -1 || av[1][0] == '\0')
 		return (print_and_return_error("Error\n", -1));
 	return (0);
 }
--- utils/ft_split_to_list.c
+++ utils/ft_split_to_list.c
@@ -6,7 +6,7 @@
 /*   By: mloeffer <mloeffer@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/16 10:46:22 by mloeffer          #+#    #+#             */
-/*   Updated: 2025/03/25 11:47:18 by mloeffer         ###   ########.fr       */
+/*   Updated: 2025/03/25 14:05:19 by mloeffer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,20 +25,21 @@ void	free_array(char **array)
 	free(array);
 }
 
-int	ft_split_to_list(char *str, char c, t_lst **a)
+int	ft_split_to_list(char *str, char c, t_lst **a, int i)
 {
 	char	**array;
-	int		i;
+	int		is_overflow;
 	int		value;
 	t_lst	*new_node;
 
+	is_overflow = 0;
 	array = ft_split(str, c);
 	if (!array)
 		return (-1);
 	i = 0;
 	while (array[i])
 	{
-		value = ft_atoi(array[i]);
+		value = ft_atoi(array[i], &is_overflow);
 		new_node = ft_lstnew(value);
 		if (!new_node)
 		{
